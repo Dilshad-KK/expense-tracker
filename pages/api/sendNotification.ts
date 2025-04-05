f87967262dc283d31a92871edf888e3697e08db2
Webmaster <dilshadkk287@gmail.com>
2025-04-05 19:07:53 +0400

    update

--- pages/api/sendNotification.ts
+++ pages/api/sendNotification.ts
@@ -3,24 +3,12 @@ import admin from "firebase-admin";
 
 // Initialize Firebase Admin SDK (only once)
 if (!admin.apps.length) {
-  // const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string);
-
-  const serviceAccount = {
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY && process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    universe_domain : process.env.FIREBASE_UNIVERSE_DOMAIN
-  };
+  const serviceAccountJson = JSON.parse(
+    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64!, 'base64').toString('utf-8')
+  );
 
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    credential: admin.credential.cert(serviceAccountJson),
   });
 }
 
@@ -42,7 +30,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     await admin.messaging().send(message);
-    return res.status(200).json({ success: true, message: "Notification sent!" });
+    console.log("key is correct")
+    // return res.status(200).json({ success: true, message: "Notification sent!" });
   } catch (error) {
     console.error("FCM Error:", error);
     return res.status(500).json({ success: false, error: "Failed to send notification", details: (error as Error).message });
