a5ce11cbe031ce5cb39637b6aecb2fbb5d2f242f
Webmaster <dilshadkk287@gmail.com>
2025-04-19 21:09:33 +0400

    update

--- pages/periods/index.tsx
+++ pages/periods/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { MdWaterDrop } from "react-icons/md";
+import GoBack from "../../components/gobackSecond";
+import moment from "moment";
 
 type PeriodData = {
   id: string;
@@ -12,10 +13,12 @@ export default function HomePage() {
   const [data, setData] = useState<PeriodData | null>(null);
   const [nextDate, setNextDate] = useState<string | null>(null);
   const [today, setToday] = useState<string>("");
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       const todayStr = new Date().toISOString().split("T")[0];
       setToday(todayStr);
 
@@ -24,7 +27,7 @@ export default function HomePage() {
       if (json?.length > 0) {
         const latest = json[0];
         setData(latest);
-
+        setLoading(false);
         const next = new Date(latest.last_period_date);
         next.setDate(next.getDate() + latest.cycle_length);
         setNextDate(next.toISOString().split("T")[0]);
@@ -34,65 +37,132 @@ export default function HomePage() {
     fetchData();
   }, []);
 
-  const handleConfirmStart = async () => {
-    if (!data) return;
+  // const handleConfirmStart = async () => {
+  //   if (!data) return;
+  //   setLoading(true);
+  //   const res = await fetch(`/api/periods?id=${data.id}`, {
+  //     method: "PUT",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({
+  //       last_period_date: today,
+  //       cycle_length: data.cycle_length,
+  //     }),
+  //   });
 
-    const res = await fetch(`/api/periods?id=${data.id}`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        last_period_date: today,
-        cycle_length: data.cycle_length,
-      }),
-    });
+  //   if (res.ok) {
+  //     const updated = await res.json();
+  //     setData(updated[0]);
+  //     const next = new Date(today);
+  //     next.setDate(next.getDate() + data.cycle_length);
+  //     setNextDate(next.toISOString().split("T")[0]);
+  //     setLoading(false);
+  //   }
+  //   setLoading(false);
+  // };
 
-    if (res.ok) {
-      const updated = await res.json();
-      setData(updated[0]);
-      const next = new Date(today);
-      next.setDate(next.getDate() + data.cycle_length);
-      setNextDate(next.toISOString().split("T")[0]);
+  const getPhaseDetails = () => {
+    if (!data?.last_period_date || !data?.cycle_length) return null;
+
+    const today = moment();
+    const lastPeriod = moment(data.last_period_date);
+    const cycleLength = data.cycle_length;
+
+    const daysSinceLastPeriod = today.diff(lastPeriod, "days");
+    const currentDayInCycle = daysSinceLastPeriod % cycleLength;
+
+    let phase = "";
+
+    if (currentDayInCycle >= 0 && currentDayInCycle <= 5) {
+      phase = "Menstrual Phase";
+    } else if (currentDayInCycle >= 6 && currentDayInCycle <= 13) {
+      phase = "Follicular Phase";
+    } else if (currentDayInCycle >= 14 && currentDayInCycle <= 16) {
+      phase = "Ovulation Phase";
+    } else {
+      phase = "Luteal Phase";
     }
+
+    return {
+      phase,
+      currentDayInCycle,
+      daysSinceLastPeriod,
+    };
   };
 
+  const getPeriodDays = () => {
+    const today = moment();
+    const nextPeriodDate = moment(data?.last_period_date).add(data?.cycle_length, "days");
+    const daysLeft = nextPeriodDate.diff(today, "days");
+    return daysLeft
+  }
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-pink-50 px-4">
-      <div className="bg-white shadow-xl rounded-2xl p-6 max-w-md w-full text-center">
-        <h1 className="text-xl font-semibold text-pink-700 mb-4">💖 Period Tracker</h1>
+    <div className="bg-[#ffffff] min-h-screen relative">
+      <div className='bg-[#514cff] px-4 py-8 flex justify-center items-center rounded-b-[24px] h-[120px]'>
+        <div className='absolute left-[-90px] z-[1000] bg-[#ffffff18] rounded-full w-[200px] h-[200px]'></div>
+        <div className='absolute left-[-30px] z-[1000] bg-[#ffffff1a] rounded-full w-[200px] h-[200px]'></div>
+        <div className='absolute left-[32px] z-[1000]'>
+          <GoBack />
+        </div>
+        <span className='text-white z-[2000] font-poppinsBold text-[18px]'>Period Details</span>
+      </div>
+      <div className="w-full text-center">
+        {loading ? 'loading' :
+          data && nextDate ? (
+            <div className="flex items-center justify-center flex-col mt-16">
+              <div className="mb-16 shadow-xl h-[300px] w-[300px] bg-white flex items-center justify-center flex-col rounded-full">
+                <div className="text-black/70 font-poppins text-[24px] mb-3">
+                  {getPhaseDetails()?.phase}
+                </div>
 
-        {data && nextDate ? (
-          <>
-            <p className="text-lg mb-2 text-pink-800">
-              🩸 Your next period is expected on <span className="font-bold">{nextDate}</span>
-            </p>
+                <div className="flex flex-col">
+                  <div className="text-black/80 text-[12px] mb-[-8px]">Day</div>
+                  <div className="text-black/80 text-[48px] font-poppinsMed">{getPhaseDetails()?.daysSinceLastPeriod}</div>
+                </div>
+              </div>
 
-            <button
-              onClick={() => router.push("/periods/update")}
-              className="mt-4 w-full bg-pink-400 text-white py-2 rounded-lg hover:bg-pink-500 transition"
-            >
-              ✏️ Update Details
-            </button>
+              <div>
+                <div className="text-black/80">Next Period Expected In {getPeriodDays()} days</div>
+                <button
+                  onClick={() => router.push("/periods/update")}
+                  className="mt-4 w-full bg-pink-400 text-white py-2 rounded-lg hover:bg-pink-500 transition"
+                >
+                  ✏️ Update Details
+                </button>
+              </div>
+              {/* <p className="text-lg mb-2 text-pink-800">
+                🩸 Your next period is expected on <span className="font-bold">{nextDate}</span>
+                {getPhaseDetails()?.phase}
+              </p> */}
 
-            {nextDate === today && (
-              <button
-                onClick={handleConfirmStart}
-                className="mt-4 w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 transition"
+              {/* <button
+                onClick={() => router.push("/periods/update")}
+                className="mt-4 w-full bg-pink-400 text-white py-2 rounded-lg hover:bg-pink-500 transition"
               >
-                ✅ Period Started Today
+                ✏️ Update Details
+              </button> */}
+
+              {/* {nextDate === today && (
+                <button
+                  onClick={handleConfirmStart}
+                  className="mt-4 w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 transition"
+                >
+                  ✅ Period Started Today
+                </button>
+              )} */}
+            </div>
+          ) : (
+            <>
+              <p className="text-pink-700 text-lg mb-4">No period data found.</p>
+              <button
+                onClick={() => router.push("/periods/update")}
+                className="bg-pink-500 text-white px-4 py-2 rounded-lg hover:bg-pink-600"
+              >
+                Add Period Info
               </button>
-            )}
-          </>
-        ) : (
-          <>
-            <p className="text-pink-700 text-lg mb-4">No period data found.</p>
-            <button
-              onClick={() => router.push("/periods/update")}
-              className="bg-pink-500 text-white px-4 py-2 rounded-lg hover:bg-pink-600"
-            >
-              Add Period Info
-            </button>
-          </>
-        )}
+            </>
+          )
+        }
       </div>
     </div>
   );
--- pages/periods/update.tsx
+++ pages/periods/update.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import GoBack from "../../components/gobackSecond";
 
 export default function UpdatePage() {
   const [id, setId] = useState<string | null>(null);
@@ -40,40 +41,49 @@ export default function UpdatePage() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-pink-50 px-4">
-      <div className="bg-white shadow-xl rounded-2xl p-6 max-w-md w-full">
-        <h1 className="text-xl font-semibold text-pink-700 text-center mb-4">🔧 Update Period Info</h1>
+    <div className="bg-[#ffffff] min-h-screen relative">
+      <div className='bg-[#514cff] px-4 py-8 flex justify-center items-center rounded-b-[24px] h-[120px]'>
+        <div className='absolute left-[-90px] z-[1000] bg-[#ffffff18] rounded-full w-[200px] h-[200px]'></div>
+        <div className='absolute left-[-30px] z-[1000] bg-[#ffffff1a] rounded-full w-[200px] h-[200px]'></div>
+        <div className='absolute left-[32px] z-[1000]'>
+          <GoBack />
+        </div>
+        <span className='text-white z-[2000] font-poppinsBold text-[18px]'>Update Period Details</span>
+      </div>
+      <div className="flex items-center justify-center h-full pt-24">
+        <div className="bg-white shadow-xl rounded-2xl p-6 max-w-md w-full">
+          <div className="space-y-4">
+            <div>
+              <label className="block text-sm font-medium text-pink-900 mb-1">Cycle Length (days)</label>
+              <input
+                type="number"
+                value={cycle}
+                onChange={(e) => setCycle(Number(e.target.value))}
+                min={1}
+                className="w-full bg-white text-black border border-pink-300 px-4 py-2 rounded-lg focus:ring-2 focus:ring-pink-400"
+              />
+            </div>
 
-        <div className="space-y-4">
-          <div>
-            <label className="block text-sm font-medium text-pink-900 mb-1">Cycle Length (days)</label>
-            <input
-              type="number"
-              value={cycle}
-              onChange={(e) => setCycle(Number(e.target.value))}
-              min={1}
-              className="w-full bg-white text-black border border-pink-300 px-4 py-2 rounded-lg focus:ring-2 focus:ring-pink-400"
-            />
+            <div>
+              <label className="block text-sm font-medium text-pink-900 mb-1">Last Period Start Date</label>
+              <input
+                type="date"
+                value={lastDate}
+                onChange={(e) => setLastDate(e.target.value)}
+                className="w-full bg-white text-black border border-pink-300 px-4 py-2 rounded-lg focus:ring-2 focus:ring-pink-400"
+              />
+            </div>
+
+            <button
+              onClick={handleSave}
+              className="w-full bg-pink-500 text-white py-2 rounded-lg hover:bg-pink-600"
+            >
+              💾 Save & Go Back
+            </button>
           </div>
-
-          <div>
-            <label className="block text-sm font-medium text-pink-900 mb-1">Last Period Start Date</label>
-            <input
-              type="date"
-              value={lastDate}
-              onChange={(e) => setLastDate(e.target.value)}
-              className="w-full bg-white text-black border border-pink-300 px-4 py-2 rounded-lg focus:ring-2 focus:ring-pink-400"
-            />
-          </div>
-
-          <button
-            onClick={handleSave}
-            className="w-full bg-pink-500 text-white py-2 rounded-lg hover:bg-pink-600"
-          >
-            💾 Save & Go Back
-          </button>
         </div>
       </div>
+
     </div>
   );
 }
