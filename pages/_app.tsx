890e104df6425da936e29b0dbb2e61bcd7592860
Webmaster <dilshadkk287@gmail.com>
2025-04-19 18:50:52 +0400

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,38 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import type { AppProps } from "next/app";
 import { useEffect } from 'react';
-import { initOneSignal } from '../lib/initOneSIgnal'; // update with your path
 
 
 export default function App({ Component, pageProps }: AppProps) {
+
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      initOneSignal();
+      import('react-onesignal').then(async (OneSignal) => {
+        await OneSignal.default.init({
+          appId: '00b19587-74cb-4d19-931d-e74f5a20e8a1',
+          notifyButton: {
+            enable: true,
+            showCredit: false,
+            prenotify: true,
+            position: 'bottom-right',
+            text: {
+              'tip.state.unsubscribed': 'Subscribe to notifications',
+              'tip.state.subscribed': "You're subscribed to notifications",
+              'tip.state.blocked': 'You have blocked notifications',
+              'message.prenotify': 'Click to subscribe to push notifications',
+              'message.action.subscribing': 'Subscribing...',
+              'message.action.subscribed': "Thanks for subscribing!",
+              'message.action.resubscribed': "You're subscribed to notifications",
+              'message.action.unsubscribed': "You won't receive notifications again",
+              'dialog.main.title': 'Manage Site Notifications',
+              'dialog.main.button.subscribe': 'SUBSCRIBE',
+              'dialog.main.button.unsubscribe': 'UNSUBSCRIBE',
+              'dialog.blocked.title': 'Unblock Notifications',
+              'dialog.blocked.message': 'Follow these instructions to allow notifications:',
+            },
+          },
+        });
+      });
     }
   }, []);
 
