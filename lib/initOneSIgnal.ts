443c60eee9078e36ad81f96eba6ee614ef24de0f
Webmaster <dilshadkk287@gmail.com>
2025-04-19 16:58:37 +0400

    update

--- lib/initOneSIgnal.ts
+++ lib/initOneSIgnal.ts
@@ -1,33 +1,48 @@
-export const initOneSignal = async () => {
-    const OneSignal = (await import('react-onesignal')).default;
+  export const initOneSignal = async () => {
+    const OneSignal = (await import("react-onesignal")).default;
   
     await OneSignal.init({
-      appId: '00b19587-74cb-4d19-931d-e74f5a20e8a1',
-      serviceWorkerPath: '/OneSignalSDKWorker.js',
-      serviceWorkerUpdaterPath: '/OneSignalSDKUpdaterWorker.js',
+      appId: "00b19587-74cb-4d19-931d-e74f5a20e8a1",
       allowLocalhostAsSecureOrigin: true,
+      autoResubscribe: true,
       notifyButton: {
         enable: true,
         prenotify: true,
         showCredit: false,
-        position: 'bottom-right',
+        position: "bottom-right",
         text: {
-          'tip.state.unsubscribed': 'Subscribe to notifications',
-          'tip.state.subscribed': 'You’re subscribed to notifications',
-          'tip.state.blocked': 'You have blocked notifications',
-          'message.prenotify': 'Click to subscribe to notifications',
-          'message.action.subscribed': 'Thanks for subscribing!',
-          'message.action.resubscribed': "You're subscribed again!",
-          'message.action.unsubscribed': "You've unsubscribed",
-          'message.action.subscribing': "Subscribing...",
-          'dialog.main.title': 'Manage Notifications',
-          'dialog.main.button.subscribe': 'SUBSCRIBE',
-          'dialog.main.button.unsubscribe': 'UNSUBSCRIBE',
-          'dialog.blocked.title': 'Unblock Notifications',
-          'dialog.blocked.message': 'Follow these instructions to allow notifications:',
+          "tip.state.unsubscribed": "Subscribe to notifications",
+          "tip.state.subscribed": "You’re subscribed to notifications",
+          "tip.state.blocked": "You have blocked notifications",
+          "message.prenotify": "Click to subscribe to notifications",
+          "message.action.subscribed": "Thanks for subscribing!",
+          "message.action.resubscribed": "You're subscribed again!",
+          "message.action.unsubscribed": "You've unsubscribed",
+          "message.action.subscribing": "Subscribing...",
+          "dialog.main.title": "Manage Notifications",
+          "dialog.main.button.subscribe": "SUBSCRIBE",
+          "dialog.main.button.unsubscribe": "UNSUBSCRIBE",
+          "dialog.blocked.title": "Unblock Notifications",
+          "dialog.blocked.message": "Follow these instructions to allow notifications:",
+        },
+      },
+      promptOptions: {
+        slidedown: {
+          prompts: [
+            {
+              type: "push",
+              autoPrompt: true,
+              delay: {
+                timeDelay: 0,
+                pageViews: 1,
+              },
+              categories: [],
+            },
+          ],
         },
       },
     });
   
-    OneSignal.Slidedown.promptPush();
+    // Manually trigger prompt
+    await OneSignal.Slidedown.promptPush();
   };
