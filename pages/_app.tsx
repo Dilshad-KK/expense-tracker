97956c0c9ed4c78c023e907d9c351e7d090b3e86
Webmaster <dilshadkk287@gmail.com>
2025-04-19 18:58:46 +0400

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,13 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import type { AppProps } from "next/app";
 import { useEffect } from 'react';
-import { initOneSignal } from '../lib/initOneSIgnal'; // update with your path
-
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    import('../lib/initOneSIgnal').then(({ initOneSignal }) => {
       initOneSignal();
-    }
+    });
   }, []);
-
   return (
     <>
       <Component {...pageProps} />
